cd6ec72b513fa94b2d08ed0b19a6ba63af6cfc43
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2025-10-26 22:47:33 +0000

    Implement first draft of lexer

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,3 +1,80 @@
-//
-// Created by dunca on 26/10/2025.
-//
+// Generate a list of tokens from an inputted file
+
+#include "tokens.h"
+#include <vector>
+#include <array>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <functional>
+
+namespace Lexer {
+    static const std::regex initialWhitespace("^\\s+");
+
+    void shrinkLine(std::string& line, std::smatch& match) {
+        line = line.substr(match.position(0) + match.length());
+    }
+
+    // Iterate over file token by token, generating a vector of tokens as it goes.
+    std::vector<Token::Token> lexFile(const std::filesystem::path& inputFile) {
+        // Create the vector to be outputted
+        std::vector<Token::Token> tokens;
+        tokens.reserve(100);
+
+        // Open the inputted file
+        std::ifstream file { inputFile };
+
+        // Iterate over the file line by line
+        std::string line {};
+        int lineCounter {0};
+        while (std::getline(file, line)) {
+            ++lineCounter;
+            // While input isn't empty
+            while (!line.empty()) {
+                std::smatch match;
+                //  if input starts with whitespace, trim it
+                if (std::regex_search(line, match, initialWhitespace)) {
+                    shrinkLine(line, match);
+                }
+                // else find longest regex match at start of input
+                else {
+                    int index {-1};
+                    bool firstMatch {true};
+                    bool matched {false};
+
+                    // Iterate over the list of patterns to find the longest match
+                    for (int i {0}; i < std::ssize(Token::patterns); ++i) {
+                        std::smatch tmp;
+                        // If the search returns anything, make sure it's the longest result
+                        if (std::regex_search(line, tmp, Token::patterns.data()[i].first)) {
+                            // Note that this relies on keywords being lower down in patterns than identifiers
+                            if (tmp.length(0)>= match.length(0) || firstMatch) {
+                                match = tmp;
+                                index = i;
+                            }
+                            firstMatch = false;
+                            matched = true;
+                        }
+                    }
+                    // If there are no matches, exit with an error message
+                    if (!matched) {
+                        std::cout << "Incorrect syntax in line " << lineCounter << "\n";
+                        exit(1);
+                    }
+
+                    // If there are, generate a token to match it and then append it to tokens
+                    tokens.emplace_back(Token::patterns.data()[index].second(match));
+
+                    shrinkLine(line, match);
+                }
+                // Iterate over list of regexes to find the longest match
+                // if no match, raise error
+                // convert match to token
+                // remove match from input
+            }
+        }
+
+
+    }
+}
--- lexer/tokens.h
+++ lexer/tokens.h
@@ -4,8 +4,10 @@
 
 #ifndef DCC_TOKENS_H
 #define DCC_TOKENS_H
+#include <regex>
 #include <variant>
 #include <string>
+#include <array>
 
 namespace Token {
     // Empty structs
@@ -25,7 +27,7 @@ namespace Token {
 
     // Non-empty structs
     struct Identifier { std::string name; };
-    struct Constant { int value; };
+    struct Constant { std::string value; };
 
     // Main token struct
     // Wrapper for a std::variant containing token types
@@ -37,5 +39,32 @@ namespace Token {
             Identifier, Constant
         > type;
     };
+
+    template <typename T>
+    inline Token tokenFactory(const T& type) {
+        return Token{type};
+    }
+
+    template <typename T>
+    inline Token tokenFactory(const T& type, const std::smatch& val) {
+        T tmp {val[0]};
+        return Token{tmp};
+    }
+
+    using regexLookup = std::pair<std::regex, std::function<Token(std::smatch)>>;
+    // Keywords must be lower down the array than patterns for this to work
+    static const std::array<regexLookup, 10> patterns {
+        {
+            {std::regex("^[a-zA-Z_]\\w*\\b"), [](const auto& m) { return tokenFactory(Identifier{}, m); }},
+            {std::regex("^[0-9]+\\b"),        [](const auto& m) { return tokenFactory(Constant{}, m); }},
+            {std::regex("^int\\b"),           [](const auto&)   { return tokenFactory(Int{}); }},
+            {std::regex("^void\\b"),          [](const auto&)   { return tokenFactory(Void{}); }},
+            {std::regex("^return\\b"),        [](const auto&)   { return tokenFactory(Return{});}},
+            {std::regex("^\\("),              [](const auto&)   { return tokenFactory(OpenParen{}); }},
+            {std::regex("^\\)"),              [](const auto&)   { return tokenFactory(CloseParen{});}},
+            {std::regex("^{"),                [](const auto&)   { return tokenFactory(OpenBrace{}); }},
+            {std::regex("^}"),                [](const auto&)   { return tokenFactory(CloseBrace{});}},
+            {std::regex("^;"),                [](const auto&)   { return tokenFactory(Semicolon{});}}
+        }};
 }
 #endif //DCC_TOKENS_H
