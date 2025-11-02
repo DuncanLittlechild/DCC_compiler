40bd6709d707eac812638e14a7799106626ddfac
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2025-11-02 12:05:08 +0000

    Integrate parser into main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <cstdio>
 
 #include "lexer/lexer.h"
+#include "parser/parser.h"
 
 constexpr std::string_view g_stopAtLexStr{ "--lex"};
 constexpr char g_stopAtLexCode {'l'};
@@ -111,6 +112,13 @@ int main(const int argc, char* argv[]) {
 
     // check stopCode
     if (stopCode == g_stopAtLexCode) {
+        return 0;
+    }
+
+    // Run parser
+    Ast::Program abstractSyntaxTree {Parser::parseProgram(tokens)};
+
+    if (stopCode == g_stopAtParseCode) {
         return 0;
     }
 
