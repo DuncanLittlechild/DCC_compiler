b75d5f4bb935bf3360fa78f7893f37c3c80119e4
DuncanLittlechild <177414121+DuncanLittlechild@users.noreply.github.com>
2025-11-15 20:33:47 +0000

    Add nodeType to better tell nodes from each other

--- assembly_generator/assembly_ast.h
+++ assembly_generator/assembly_ast.h
@@ -7,8 +7,27 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <array>
 
 namespace AAst {
+
+	enum NodeType {
+		AstT,
+		ProgramT,
+		FunctionT,
+		IdentifierT,
+		InstructionT,
+		OperandT,
+		MovInstructionT,
+		RetInstructionT,
+		ImmOperandT,
+		RegisterOperandT,
+		NodeTypeLength
+	};
+
+	constexpr std::array<std::string, NodeTypeLength> nodeTypeStrings {"Ast", "Program", "Function", "Identifier",
+		"MovInstruction", "RetInstruction", "Immediate", "RegisterOperand"};
+	static_assert(std::size(nodeTypeStrings) == NodeTypeLength && "nodeTypeStrings is a different length to NodeType");
 
 	class Ast;
 	class Program;
@@ -27,68 +46,81 @@ namespace AAst {
 	// <<operator as friend
 	// virtual destructor
 	class Ast {
+		NodeType m_nodeType {AstT};
 	public:
-		virtual std::string identify() const = 0;
 		virtual ~Ast() = default;
+
 		friend std::ostream& operator<<(std::ostream& out, const Ast& ast) {
 			out << ast.identify();
 			return out;
 		}
+
+		virtual const std::string& identify() const {
+			return nodeTypeStrings[m_nodeType];
+		}
+
+		virtual const NodeType& type() const {
+			return m_nodeType;
+		}
 	};
 
 	// Base class from which to derive types of Operand
-	class Operand : public Ast {};
+	class Operand : public Ast {
+		NodeType m_nodeType {OperandT};
+	public:
+		virtual const std::string opString() const = 0;
+	};
 
 	// Container for an int
 	class ImmOperand : public Operand {
 		int m_value;
+
+		NodeType m_nodeType {ImmOperandT};
 	public:
 		ImmOperand(int value)
 			: m_value{value}
 		{}
 
-		std::string identify() const override {
-			return std::to_string(m_value);
-		}
-
 		int value() const { return m_value; }
+
+		const std::string opString() const override { return "$" + std::to_string(m_value); }
 	};
 
 	// Container for a register name - initially blank
 	class RegisterOperand : public Operand {
 		// Name/address of the register
 		std::string m_destination;
+
+		NodeType m_nodeType {RegisterOperandT};
 	public:
 		RegisterOperand(const std::string& destination)
 			: m_destination{destination}
 		{}
 
-		std::string identify() const override {
-			return "RegisterOperand";
-		}
-
 		//Returns a const reference to the destination address
 		const std::string& destination() const { return m_destination; }
 		//Returns a copy of the destination address
 		std::string getDestination() const { return m_destination; }
+
+		const std::string opString() const override { return m_destination; }
 	};
 
 	// Base class from which to derive types of instruction
-	class Instruction : public Ast {};
+	class Instruction : public Ast {
+		NodeType m_nodeType {InstructionT};
+	};
 
 	// Container for two pointers to operands
 	class MovInstruction : public Instruction {
 		std::unique_ptr<Operand> m_toMove;
 		std::unique_ptr<RegisterOperand> m_destination;
+
+		NodeType m_nodeType {MovInstructionT};
 	public:
 		MovInstruction(std::unique_ptr<Operand>&& toMove, std::unique_ptr<RegisterOperand>&& destination)
 			: m_toMove{std::move(toMove)}
 			, m_destination{std::move(destination)}
 		{}
-
-		std::string identify() const override {
-			return "MovInstruction";
-		}
 
 		const Operand& toMove() { return *m_toMove; }
 		const Operand& destination() { return *m_destination; }
@@ -96,24 +128,19 @@ namespace AAst {
 
 	// Empty class to represent return
 	class RetInstruction : public Instruction {
-	public:
-		std::string identify() const override {
-			return "RetInstruction";
-		}
+		NodeType m_nodeType {RetInstructionT};
 	};
 
 
 	// Container for std::string
 	class Identifier : public Ast {
 		std::string m_identifier;
+
+		NodeType m_nodeType {IdentifierT};
 	public:
 		Identifier(std::string&& identifier)
 			: m_identifier{std::move(identifier)}
 		{}
-
-		std::string identify() const override {
-			return m_identifier;
-		}
 
 		const std::string& identifier() const { return m_identifier; }
 	};
@@ -122,17 +149,15 @@ namespace AAst {
 	class Function : public Ast {
 		std::unique_ptr<Identifier> m_identifier;
 		std::vector<std::unique_ptr<Instruction>> m_instructions;
+
+		NodeType m_nodeType {FunctionT};
 	public:
 		Function(std::unique_ptr<Identifier>&& identifier, std::vector<std::unique_ptr<Instruction>>&& instructions)
 			: m_identifier{std::move(identifier)}
 			, m_instructions{std::move(instructions)}
 		{}
 
-		std::string identify() const override {
-			return "Function";
-		}
-
-		const Identifier& identifier() { return *m_identifier; }
+		const Identifier& identifier() const { return *m_identifier; }
 
 		const std::vector<std::unique_ptr<Instruction>>& instructions() const { return m_instructions; }
 	};
@@ -140,14 +165,11 @@ namespace AAst {
 	// Container for pointer to function
 	class Program : public Ast {
 		std::unique_ptr<Function> m_function;
+		NodeType m_nodeType {ProgramT};
 	public:
 		Program(std::unique_ptr<Function>&& a_function)
 			: m_function{std::move(a_function)}
 		{}
-
-		std::string identify() const override {
-			return "Program";
-		}
 
 		const Function& function() const { return *m_function; }
 	};
