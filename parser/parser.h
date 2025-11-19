f9dc78695f2e1f7cfc06882b267118cb0e799dd3
Duncan Littlechild <177414121+DuncanLittlechild@users.noreply.github.com>
2025-11-19 14:06:06 +0000

    Refactor parser.h to add new parsing functions

--- parser/parser.h
+++ parser/parser.h
@@ -14,18 +14,29 @@ namespace Parser {
 
 	Token::Token& expect(auto& expected, VectorAndIterator& tokens);
 
-	// Parse Integer values and return a pointer
-	std::unique_ptr<Ast::IntConstant> parseInt (Token::Token& token);
+	std::unique_ptr<Ast::Identifier> parseIdentifier(VectorAndIterator& tokens);
 
+	std::unique_ptr<Ast::UnaryOperator> parseUnaryOperator (auto& constantToken);
+
+	// Construct the unary operator constant
+	// This can be nested an arbitrary number of times
+	std::unique_ptr<Ast::OperatorConstant> parseUnaryOperatorConstant(auto& currentTokenName, VectorAndIterator& tokens);
+
+	// Parse Integer values and return a pointer
+	std::unique_ptr<Ast::IntConstant> parseIntConstant (Token::Token& token);
+
+	// Helper to work out what type of cosntatn token to create
 	std::unique_ptr<Ast::Constant> parseConstant(VectorAndIterator& tokens);
 
-	std::unique_ptr<Ast::Statement> parseStatement(VectorAndIterator& tokens);
+	std::unique_ptr<Ast::KeywordStatement> parseKeywordStatement (const std::string& keyword, VectorAndIterator& tokens);
 
-	std::unique_ptr<Ast::Identifier> parseIdentifier(VectorAndIterator& tokens);
+	// Statements are complete lines that come before semicolons in C
+	// Helper function to select the correct type of statement
+	std::unique_ptr<Ast::Statement> parseStatement(VectorAndIterator& tokens);
 
 	std::unique_ptr<Ast::Function> parseFunction(VectorAndIterator& tokens);
 
 	Ast::Program parseProgram(std::vector<Token::Token>& t);
-
 }
+
 #endif //DCC_PARSER_H
